2cee420bc86b757103b374a138ad1cf7d7a890ab
Umehani <160249886+Umehanishah@users.noreply.github.com>
2025-04-13 20:08:49 +0500

    Add files via upload

--- 01_expressions/01_dicesimulator.py
+++ 01_expressions/01_dicesimulator.py
@@ -1,33 +1,29 @@
 import random
-import streamlit as st # type: ignore
+import streamlit as st
 
 st.title("Dice Simulator 🎲")
 
 NUM_SIDES = 6
 
 def roll_dice():
-   
-    die1: int = random.randint(1, NUM_SIDES)
-    die2: int = random.randint(1, NUM_SIDES)
-    total: int = die1 + die2
-    print(f"🎯 You rolled: 🎲 {die1} + 🎲 {die2} = 💥 {total}")
+    die1 = random.randint(1, NUM_SIDES)
+    die2 = random.randint(1, NUM_SIDES)
+    total = die1 + die2
+    st.write(f"🎯 You rolled: 🎲 {die1} + 🎲 {die2} = 💥 {total}")
 
 def main():
-    print("🎉 Welcome to the Dice Simulator!\n")
+    st.write("🎉 Welcome to the Dice Simulator!\n")
     
-    die1: int = 10  # This variable is local to main()
-    print("🔍 die1 in main() starts as: " + str(die1) + "\n")
+    die1 = 10  # This variable is local to main()
+    st.write("🔍 die1 in main() starts as: " + str(die1) + "\n")
 
-  
-    print("🔁 Rolling dice 3 times...\n")
+    st.write("🔁 Rolling dice 3 times...\n")
     roll_dice()
     roll_dice()
     roll_dice()
 
-    
-    print("\n📌 die1 in main() after rolling dice is still: " + str(die1))
-    print("\n🎲 Thanks for playing!")
-
+    st.write("\n📌 die1 in main() after rolling dice is still: " + str(die1))
+    st.write("\n🎲 Thanks for playing!")
 
 if __name__ == '__main__':
     main()
